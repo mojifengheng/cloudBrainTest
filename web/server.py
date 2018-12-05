5ebe122ac9c8d89fef33155b2932f21e9d55b518
zhangzhi <2673549804@qq.com>
2018-12-05 14:17:32 +0800

    this version include (1) virtual env and (2) 60% of web_login

---
+++ web/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["POST", "GET"])
+def login():
+    return render_template("index.html")
+
+
+@app.route('/checkForm', methods=["POST", "GET"])
+def checkForm():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    cheak_flag = "False"
+    with open("password.json", "r") as f:
+        data = json.loads(f.read())
+        if username in data.keys():
+            if password == data[username]:
+                cheak_flag = "True"
+    return cheak_flag
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',
+            port='1817',
+            debug=True)
---
+++ web/static/js/login.js
@@ -0,0 +1,31 @@
+function checkForm()
+{
+  var username = $("input[name='username']").val();
+  var password = $("input[name='password']").val();
+  //alert(username)
+  //alert(password)
+  $.ajax({
+  //提交数据的类型 POST GET
+    type:"POST",
+  //提交的网址
+    url:$SCRIPT_ROOT + "checkForm",
+   //	url:$SCRIPT_ROOT + "Robot/" + p_from_aa + "/Start",
+  //提交的数据
+    data:{"username":username,"password":password},
+  //返回数据的格式
+    datatype: "json",//"xml", "html", "script", "json", "jsonp", "text".
+  //在请求之前调用的函数
+  //成功返回之后调用的函数
+    success:function(data){
+    },
+  //调用执行后调用的函数
+    complete: function(XMLHttpRequest, textStatus){
+      text = XMLHttpRequest.responseText;
+      alert(text)
+    },
+  //调用出错执行的函数
+    error: function(){
+    //请求出错处理
+    }
+  });
+}
