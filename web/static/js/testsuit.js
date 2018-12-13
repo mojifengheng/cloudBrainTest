1b3e3f73c6df3abd236c40c5a1b7cd302f852561
zhangzhi <2673549804@qq.com>
2018-12-13 17:18:18 +0800

    refactor the testsuit table by using better func

--- web/static/js/testsuit.js
+++ web/static/js/testsuit.js
@@ -2,85 +2,47 @@ function loadAllInTestSuit(){
   var urlData = getUrlData();
   var userData = getUserData(urlData["urlUserName"]);
   loadNav(urlData, userData);
-  //loadTestsuit(project);
+  loadTestsuit(urlData, userData);
 }
 
-function loadTestsuit(project){
-  if (project == null){
-    project = userData["project"][0]["projectName"];
+
+function loadTestsuit(urlData, userData){
+  var tableID = "tbody-suit";
+  var tableColName = ["suitName", "suitDiscribe", "suitCreatTime", "operation"]
+  loadTestsuitTable(tableID, tableColName, urlData, userData);
+}
+
+
+function loadTestsuitTable(tableID, tableColName, urlData, userData){
+  if (urlData["urlProjectName"] == null){
+    return;
   }
+  var urlProjectName = urlData["urlProjectName"];
   for(var i = 0; i < userData["project"].length; ++i){
-    var projectName = userData["project"][i]["projectName"];
-    if (project == projectName){
-      var allSuit = userData["project"][i]["suit"]
-      for(var j = 0; j < allSuit.length; ++j){
-        var suit = allSuit[j];
-        addSuitTr(suit);
-      }
+    var userProjectName = userData["project"][i]["projectName"];
+    if (urlProjectName == userProjectName){
+      var project = userData["project"][i];
+      addTrToTestsuit(tableID, tableColName, project);
     }
   }
 }
 
-function addSuitTr(suit){
-  var tr = document.createElement("tr");
-  var tdData = [suit["suitName"], suit["suitDiscribe"], suit["suitCreatTime"]];
-  for(var i = 0; i < 3; ++i){
-    addSuitTd(tr, tdData[i]);
+function addTrToTestsuit(tableID, tableColName, project){
+  var tbody = document.getElementById(tableID);
+  for (var i = 0; i < project["suit"].length; ++i){
+    var tr = createHtmlLabel("tr");
+    for(var j = 0; j < tableColName.length; ++j){
+      if (tableColName[j] == "operation"){
+        var operation = ["添加测试用例", "执行测试用例集", "修改测试用例集", "删除测试用例集"];
+        var clickOperation = ["#", "#", "#", "#"]
+        addOperation(tr, operation, clickOperation);
+      } else {
+        var tdText = project["suit"][i][tableColName[j]];
+        var td = createHtmlLabel("td", null, tdText);
+        addChildLabel(tr, td);
+      }
+    }
+    addChildLabel(tbody, tr);
   }
-  var operation = ["添加测试用例", "执行测试用例集", "修改测试用例集", "删除测试用例集"];
-  addOperation(tr, operation);
-  var tbody = document.getElementById("tbody-suit");
-  addChildNode(tbody, tr);
-}
 
-function addSuitTd(label, data){
-  var td = document.createElement("td");
-  td.innerHTML = data;
-  addChildNode(label, td);
-}
-
-function addOperation(label, operation){
-  var td = document.createElement("td");
-  var div_1 = addDiv(td, "ticket-actions col-md-2");
-  var div_2 = addDiv(div_1, "btn-group dropdown");
-  addSuitButton(div_2);
-  var div_3 = addDiv(div_2, "dropdown-menu");
-  for(var i = 0; i < operation.length; ++i){
-    var a = addSuitA(div_3, operation[i]);
-    addSuitI(a,"fa fa-reply fa-fw");
-  }
-  addChildNode(label, td);
-}
-
-function addDiv(label, itClass){
-  var div = document.createElement("div");
-  div.setAttribute("class", itClass);
-  addChildNode(label, div);
-  return div;
-}
-
-function addSuitButton(label){
-  var button = document.createElement("button");
-  button.setAttribute("type", "button");
-  button.setAttribute("class", "btn btn-success dropdown-toggle btn-sm");
-  button.setAttribute("data-toggle","dropdown");
-  button.setAttribute("aria-haspopup","true");
-  button.setAttribute("aria-expanded", "false");
-  button.innerHTML = "操作";
-  addChildNode(label, button);
-}
-
-function addSuitA(label, data){
-  var a = document.createElement("a");
-  a.setAttribute("class", "dropdown-item");
-  a.setAttribute("href", "#")
-  a.innerHTML = data;
-  addChildNode(label, a);
-  return a;
-}
-
-function addSuitI(label, itClass){
-  var i = document.createElement('i');
-  i.setAttribute("class", itClass);
-  addChildNode(label, i);
 }
---
+++ web/static/js/util.js
@@ -0,0 +1,54 @@
+//util getHtmlLabel: get Html Label With attribute
+function createHtmlLabel(label, labelAttributeDict=null, text=null){
+  var label = document.createElement(label);
+  for(var key in labelAttributeDict){
+    label.setAttribute(key, labelAttributeDict[key]);
+  }
+  if (text != null){
+    label.innerHTML = text;
+  }
+  return label;
+}
+
+
+//util addChildLabel: add childLabel to fatherLabel
+function addChildLabel(fatherLabel, childLabel){
+  fatherLabel.appendChild(childLabel)
+}
+
+function addOperation(label, operation, clickOperation){
+  var td = createHtmlLabel("td", null, null);
+  var div1Attribute = {"class":"ticket-actions col-md-2"};
+  var div1 = createHtmlLabel("div", div1Attribute);
+  addChildLabel(td, div1);
+
+  var div2Attribute = {"class":"btn-group dropdown"};
+  var div2 = createHtmlLabel("div", div2Attribute);
+  addChildLabel(div1, div2);
+
+  var buttonAttribute = {
+    "type":"button",
+    "class":"btn btn-success dropdown-toggle btn-sm",
+    "data-toggle":"dropdown",
+    "aria-haspopup":"true",
+    "aria-expanded":"false",
+  };
+  var buttonText = "操作";
+  var button = createHtmlLabel("button", buttonAttribute, buttonText);
+  addChildLabel(div2, button);
+
+  var div3Attribute = {"class":"dropdown-menu"}
+  var div3 = createHtmlLabel("div", div3Attribute);
+
+  for(var i = 0; i < operation.length; ++i){
+    var aHrefUrl = clickOperation[i];
+    var aAttribute = {"class":"dropdown-item", "href":aHrefUrl};
+    var a = createHtmlLabel("a", aAttribute, aHrefUrl);
+    addChildLabel(div3, a);
+
+    var iAttribute = {"class": "fa fa-reply fa-fw"};
+    var iLabel = createHtmlLabel("i", iAttribute);
+    addChildLabel(div3, iLabel);
+  }
+  addChildLabel(label, td);
+}
